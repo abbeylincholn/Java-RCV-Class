6742110f789f9e5f965f86616660a47a0f80eb90
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2024-09-15 11:51:17 +0100

    Try, Catch and Finally

---
+++ src/main/java/TryCatch/TryCatchDemo.java
@@ -0,0 +1,28 @@
+package TryCatch;
+
+public class TryCatchDemo {
+
+	public static void main(String[] args) {
+
+		try {
+
+			int i = 10;
+			System.out.println("Before division");
+			System.out.println(i / 0);
+
+		} catch (Throwable t) {
+
+			System.out.println(t.getMessage());
+			System.out.println(t.getCause());
+			System.out.println(t.getStackTrace());
+			t.printStackTrace();
+
+		} finally {
+			System.out.println("This is finally block");
+		}
+
+		System.out.println("After division");
+
+	}
+
+}
